1a496bda83dd898d5ab583611f60f5c54bdc9038
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-10-06 00:23:37 +0530

    printed the alphabets

---
+++ src/CountAlphabets.java
@@ -0,0 +1,12 @@
+public class CountAlphabets {
+    public static void main(String[] args) {
+
+        //Creating an Array to store the alphabets
+        String[] alphabets = new String[] {"A", "B", "C", "D", "E", "F"};
+
+        //Iterating the array to count the alphabets
+        for (String alphabet : alphabets) {
+            System.out.println(alphabet);
+        }
+    }
+}
