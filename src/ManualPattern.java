5b5143bfbc5efad162deecc13defedeeb17fb4ff
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-10-06 12:42:56 +0530

    added commets

--- src/ManualPattern.java
+++ src/ManualPattern.java
@@ -1,10 +1,13 @@
 public class ManualPattern {
     public static void main(String[] args) {
 
+        //outer loop traverse starts with 5 and decreases by 1 each time
         for (int i = 5; i >= 1; i--) {
+            //inner loop traverse upto i
             for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
+            //It goes to the next lines after printing each row
             System.out.println();
         }
     }
