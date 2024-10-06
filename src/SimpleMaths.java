b2eb782c3233bb44d507b540eeff96163ee498f4
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-10-06 12:15:31 +0530

    simple maths

---
+++ src/SimpleMaths.java
@@ -0,0 +1,11 @@
+public class SimpleMaths {
+    public static void main(String[] args) {
+
+        int A = 6;
+        int B = 3;
+        System.out.println(A + B);
+        System.out.println(A - B);
+        System.out.println(A * B);
+        System.out.println(A / B);
+    }
+}
