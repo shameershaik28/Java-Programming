6a7b708299bcdde20bed2379f0a8b22655cb1f19
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-10-06 12:40:14 +0530

    optimised the code

--- src/ManualPattern.java
+++ src/ManualPattern.java
@@ -1,14 +1,11 @@
 public class ManualPattern {
     public static void main(String[] args) {
 
-         int A = 5;
-         for(int i=0; i<5; i++) {
-             for(int j=0; j<A; j++) {
-                 System.out.print("*");
-             }
-             System.out.println();
-             A--;
-         }
-         System.out.println();
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
