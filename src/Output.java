739efc6c707676eb7a4fc40c3c6327405f666377
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-10-07 14:59:49 +0530

    Printed the output

---
+++ src/Output.java
@@ -0,0 +1,7 @@
+public class Output {
+    public static void main(String[] args) {
+            System.out.println("Hello");
+            System.out.println();
+            System.out.println("World!");
+        }
+    }
