f7002c2d867d6e17308296fe703442e1eac5cb00
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-10-07 14:57:55 +0530

    Printed Hello world

---
+++ src/HelloWorld2.java
@@ -0,0 +1,7 @@
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        int A = 40, B =50;
+        System.out.println("Hello "+A);
+        System.out.println(B+" World!");
+    }
+}
