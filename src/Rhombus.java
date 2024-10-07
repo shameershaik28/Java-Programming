6b0630e0310d4145b5f2a3ee7509c7131decad0c
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-10-07 15:12:27 +0530

    printed Rhombus

---
+++ src/Rhombus.java
@@ -0,0 +1,14 @@
+public class Rhombus {
+    public static void main(String[] args) {
+        for(int i = 1; i <=3; i++){
+            if(i==2)
+            {
+                System.out.println("*"+" "+"*");
+            }
+            else {
+                System.out.println(" "+"*"+" ");
+            }
+
+        }
+    }
+}
