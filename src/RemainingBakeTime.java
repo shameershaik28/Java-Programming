f0e4b53e755c81845d15515e5e14bf3a9658df56
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-10-07 15:30:45 +0530

    Added comments

--- src/RemainingBakeTime.java
+++ src/RemainingBakeTime.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
 
 public class RemainingBakeTime {
+    //function to handle the remaining time, T is the actual time
     public int timeLeft(int T)
     {
        int TotalTime = 40;
+       //edge case
        if(T<0)
        {
            return TotalTime;
        }
        else
-        return TotalTime-T;
+        return TotalTime-T; // total time - actual time = remaining Time and it returns back to the main method.
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
