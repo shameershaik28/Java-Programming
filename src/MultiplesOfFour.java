b42b0ebe0bc8ad4fc677d01c575dca43f43f8228
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-10-09 12:50:09 +0530

    Multiple of 4

---
+++ src/MultiplesOfFour.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MultiplesOfFour {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        // Edge case: N should be between 1 and 1000000
+        if(N==0 || N >10000)
+        {
+            return;
+        }
+
+        for(int i =1; i<=N ; i++)
+        {
+            if(i%4==0)
+            {
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
