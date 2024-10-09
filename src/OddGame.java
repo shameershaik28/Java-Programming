2e1382346469e656cd5f409d3e1daf1abde351f4
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-10-09 20:37:50 +0530

    printed the odd values

---
+++ src/OddGame.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class OddGame {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for(int i =1; i<=N; i++)
+        {
+            if(i%2!=0)
+            {
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
