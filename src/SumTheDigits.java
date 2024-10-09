a27c872fc17f7934278fe66171e9b67d1b020af5
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-10-09 22:45:27 +0530

    Updated the code with edge cases

--- src/SumTheDigits.java
+++ src/SumTheDigits.java
@@ -4,7 +4,10 @@ public class SumTheDigits {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt(); // number of test cases
-
+        if (T == 0 || T>1000)
+         {
+          return;
+         }
         for (int i = 0; i < T; i++) {
             int N = sc.nextInt(); // input for each test case
 
