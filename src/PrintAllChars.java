5fe079418715ec099640c3e3de0a0d98df8e36cd
shameershaik28 <102400953+shameershaik28@users.noreply.github.com>
2024-10-26 20:15:05 +0530

    It takes and string and prints the characters line by line

---
+++ src/PrintAllChars.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class PrintAllChars {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        for(int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
+        }
+
+    }
+}
