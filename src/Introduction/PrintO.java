b80daba4ec624030d31c8b1a13354a98e0ffffc2
Shameer Shaik <shameershaik28@gmail.com>
2026-09-20 17:04:13 +0530

    Add Print O problem

--- src/Introduction/PrintO.java
+++ src/Introduction/PrintO.java
@@ -1,4 +1,29 @@
 package Introduction;
+/*
+Problem: Print O
+
+Question:
+Print the following pattern in output:
+
+******
+*    *
+*    *
+*    *
+******
+*/
 
 public class PrintO {
+    public static void main(String[] args) {
+        for(int i=1; i<=5; i++)
+        {
+            if(i==1 || i==5)
+            {
+                System.out.println("******");
+            }
+            else
+            {
+                System.out.println("*    *");
+            }
+        }
+    }
 }
