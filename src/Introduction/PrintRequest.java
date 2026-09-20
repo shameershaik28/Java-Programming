51a973766dda95d132ce14ddce8cff19dc62669c
Shameer Shaik <shameershaik28@gmail.com>
2026-09-20 17:09:22 +0530

    Add Print Request problem

---
+++ src/Introduction/PrintRequest.java
@@ -0,0 +1,22 @@
+package Introduction;
+
+/*
+Problem: Print Request
+
+Question:
+Print the following message in text:
+
+"Dear Compiler"
+"----Never Give me bugs"
+"----If bugs are there, please help me to fix It"
+"----Thank You"
+*/
+
+public class PrintRequest {
+    public static void main(String[] args) {
+        System.out.println("Dear Compiler");
+        System.out.println("----Never Give me bugs");
+        System.out.println("----If bugs are there, please help me to fix It");
+        System.out.println("----Thank You");
+    }
+}
