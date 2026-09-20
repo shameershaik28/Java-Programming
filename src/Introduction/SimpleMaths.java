f40072e8a1e1907785b031ae971233fc7d853fd5
Shameer Shaik <shameershaik28@gmail.com>
2026-09-20 16:44:20 +0530

    Add Simple Maths problem

---
+++ src/Introduction/SimpleMaths.java
@@ -0,0 +1,24 @@
+package Introduction;
+
+/*
+Perform the following operations on 6 and 3 and print their respective outputs in different lines.
+
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Double Division
+ */
+
+public class SimpleMaths {
+
+    public static void main(String[] args) {
+
+        int k = 6;
+        int j = 3;
+
+        System.out.println(k + j);
+        System.out.println(k - j);
+        System.out.println(k * j);
+        System.out.println(k / j);
+    }
+}
