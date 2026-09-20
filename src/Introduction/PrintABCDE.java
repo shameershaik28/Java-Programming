b7066a121854b9bdd690a7387a48e099cbd09cf2
Shameer Shaik <shameershaik28@gmail.com>
2026-09-20 16:35:09 +0530

    Add PrintABCDE problem

---
+++ src/Introduction/PrintABCDE.java
@@ -0,0 +1,15 @@
+package Introduction;
+
+public class PrintABCDE {
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+
+        for(char i = 'A'; i<= 'E'; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
