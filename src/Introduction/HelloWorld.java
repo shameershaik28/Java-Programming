d8c24048d2b85f3ad87290537f6bda5da38c5853
Shameer Shaik <shameershaik28@gmail.com>
2026-09-20 16:58:00 +0530

    Add Hello World

---
+++ src/Introduction/HelloWorld.java
@@ -0,0 +1,17 @@
+package Introduction;
+
+/*
+
+Question:
+Print "Hello World !" in the output.
+
+*/
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World !");
+
+    }
+}
