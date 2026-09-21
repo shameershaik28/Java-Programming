e0a351e026bbb8e38fbc7b856a9eb61f43c7957a
Shameer Shaik <shameershaik28@gmail.com>
2026-09-21 14:49:49 +0530

    Add Hello World 4 problem

---
+++ src/Introduction/PrintHelloWorld.java
@@ -0,0 +1,15 @@
+package Introduction;
+/*
+Question:
+Print the following text:
+
+Hello 40
+50 World !
+*/
+
+public class PrintHelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello 40");
+        System.out.println("50 World !");
+    }
+}
