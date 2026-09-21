27ae64b6143e2bb2455858ec20e1a79123afdf45
Shameer Shaik <shameershaik28@gmail.com>
2026-09-21 14:51:31 +0530

    Add Print Rectangle problem

---
+++ src/Introduction/PrintRectangle.java
@@ -0,0 +1,15 @@
+package Introduction;
+/*
+Problem: Print Rectangle
+
+Question:
+Print the following pattern in output:
+
+**********
+*        *
+*        *
+**********
+*/
+
+public class PrintRectangle {
+}
