a622d6945c54beb76cb8bfef51922e32060c865c
Shameer Shaik <shameershaik28@gmail.com>
2026-09-21 14:56:55 +0530

    Add Print Rectangle problem

--- src/Introduction/PrintRectangle.java
+++ src/Introduction/PrintRectangle.java
@@ -12,4 +12,16 @@ Print the following pattern in output:
 */
 
 public class PrintRectangle {
+    public static void main(String[] args) {
+
+        for(int i=1; i<=4; i++)
+        {
+            if(i==1 || i==4)
+            {
+                System.out.println("**********");
+            }
+            else
+                System.out.println("*        *");
+        }
+    }
 }
--- src/Introduction/PrintRhombus.java
+++ src/Introduction/PrintRhombus.java
@@ -1,4 +1,26 @@
 package Introduction;
+/*
+Problem: Print Rhombus
+
+Question:
+Print the following pattern in output:
+
+ *
+* *
+ *
+
+*/
 
 public class PrintRhombus {
+    public static void main(String[] args) {
+        for(int i=1; i<=3; i++)
+        {
+            if(i==1 || i==3)
+            {
+                System.out.println(" *");
+            }
+            else
+                System.out.println("* *");
+        }
+    }
 }
