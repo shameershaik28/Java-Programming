ac88300ff86304e09c0d8d3d13bbbc47a99cba92
Shameer Shaik <shameershaik28@gmail.com>
2026-09-21 15:03:32 +0530

    Add Even Numbers problem

---
+++ src/Introduction/EvenNumbers.java
@@ -0,0 +1,15 @@
+package Introduction;
+/*
+Problem: Even Numbers
+
+Question:
+Print the following:
+
+2 Two 4 Four 6 Six
+*/
+
+public class EvenNumbers {
+    public static void main(String[] args) {
+        System.out.println("2 Two 4 Four 6 Six");
+    }
+}
