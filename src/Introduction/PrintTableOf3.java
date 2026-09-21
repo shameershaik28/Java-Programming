5df137daeedfc79b7fa9c9220970167a5e08361b
Shameer Shaik <shameershaik28@gmail.com>
2026-09-21 15:00:00 +0530

    Add Print Table of 3 problem

--- src/Introduction/PrintTableOf3.java
+++ src/Introduction/PrintTableOf3.java
@@ -1,4 +1,21 @@
 package Introduction;
+/*
+Problem: Print Table of 3
+
+Question:
+Print the first five multiples of 3:
+
+3 6 9 12 15
+*/
 
 public class PrintTableOf3 {
+    public static void main(String[] args) {
+
+        for (int j = 1; j <=5; j++) {
+            if(j==5)
+                System.out.print(3*j);
+            else
+                System.out.print(3*j+" ");
+        }
+    }
 }
